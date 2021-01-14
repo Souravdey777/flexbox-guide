cb64dd2732f289bc15ac5ac0678049bda74c4979
Souravdey777 <piyush.kolkata@gmail.com>
2021-01-14 21:32:06 +0530

    Star the repo text added

--- src/Components/FlexHeading.js
+++ src/Components/FlexHeading.js
@@ -8,6 +8,7 @@ function FlexHeading(props) {
             <div className={Styles.pageSubHeading}>By <a className={Styles.link} href="https://github.com/Souravdey777" target="_blank" rel="noreferrer">Sourav Dey</a></div>
             <div className={Styles.pageDescription}>A Guide for the concept of flexbox and responsive design. Set the attributes as needed and copy the CSS code.</div>
             <div className={Styles.pageDescription}>CSS Flexible Box Layout is a module of CSS that defines a CSS box model optimized for user interface design, and the layout of items in one dimension.</div>
+            <div className={Styles.pageDescription}>Show some love ⭐ Star the Repository and support the project</div>
             <div className={Styles.githublink}><a href="https://github.com/Souravdey777/Flexbox-Guide" target="_blank" rel="noreferrer"><img style={{ borderRadius: "4px" }} title="FlexBox-Guide Github Repo" alt="Flexbox-Guide Github Repo" src="https://img.shields.io/badge/Source%20Code-ffa500?style=flat-square&logo=github&logoColor=fff&label=Github&labelColor=333" /></a></div>
         </div>
     );
