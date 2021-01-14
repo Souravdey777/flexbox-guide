7e3c433b9b1e50d07c185835f6e3dae8a08cd457
Souravdey777 <piyush.kolkata@gmail.com>
2021-01-14 13:21:59 +0530

    github badge ui fixed

--- src/Components/FlexHeading.js
+++ src/Components/FlexHeading.js
@@ -7,7 +7,7 @@ function FlexHeading(props) {
             <div className={Styles.pageHeading}>Flexbox-Guide</div>
             <div className={Styles.pageSubHeading}>By <a className={Styles.link} href="https://github.com/Souravdey777">Sourav Dey</a></div>
             <div className={Styles.pageDescription}>CSS Flexible Box Layout is a module of CSS that defines a CSS box model optimized for user interface design, and the layout of items in one dimension.</div>
-            <div className={Styles.githublink}><a href="https://github.com/Souravdey777/Flexbox-Guide"><img title="FlexBox-Guide Github Repo" alt="Flexbox-Guide Github Repo" src="https://img.shields.io/badge/Source%20Code-ffa500?style=flat&logo=github&logoColor=fff&label=Github&labelColor=333" /></a></div>
+            <div className={Styles.githublink}><a href="https://github.com/Souravdey777/Flexbox-Guide" target="_blank"><img style={{ borderRadius: "4px" }} title="FlexBox-Guide Github Repo" alt="Flexbox-Guide Github Repo" src="https://img.shields.io/badge/Source%20Code-ffa500?style=flat-square&logo=github&logoColor=fff&label=Github&labelColor=333" /></a></div>
         </div>
     );
 }
