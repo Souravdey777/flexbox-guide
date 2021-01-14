5d8f820131bd505d6b65df6c37718651105e67b7
Souravdey777 <piyush.kolkata@gmail.com>
2021-01-14 20:26:23 +0530

    target blank added in url

--- src/Components/FlexHeading.js
+++ src/Components/FlexHeading.js
@@ -5,10 +5,10 @@ function FlexHeading(props) {
     return (
         <div className={Styles.rootFlexHeading}>
             <div className={Styles.pageHeading}>Flexbox-Guide</div>
-            <div className={Styles.pageSubHeading}>By <a className={Styles.link} href="https://github.com/Souravdey777">Sourav Dey</a></div>
+            <div className={Styles.pageSubHeading}>By <a className={Styles.link} href="https://github.com/Souravdey777" target="_blank" rel="noreferrer">Sourav Dey</a></div>
             <div className={Styles.pageDescription}>A Guide for the concept of flexbox and responsive design. Set the attributes as needed and copy the CSS code.</div>
             <div className={Styles.pageDescription}>CSS Flexible Box Layout is a module of CSS that defines a CSS box model optimized for user interface design, and the layout of items in one dimension.</div>
-            <div className={Styles.githublink}><a href="https://github.com/Souravdey777/Flexbox-Guide" target="_blank"><img style={{ borderRadius: "4px" }} title="FlexBox-Guide Github Repo" alt="Flexbox-Guide Github Repo" src="https://img.shields.io/badge/Source%20Code-ffa500?style=flat-square&logo=github&logoColor=fff&label=Github&labelColor=333" /></a></div>
+            <div className={Styles.githublink}><a href="https://github.com/Souravdey777/Flexbox-Guide" target="_blank" rel="noreferrer"><img style={{ borderRadius: "4px" }} title="FlexBox-Guide Github Repo" alt="Flexbox-Guide Github Repo" src="https://img.shields.io/badge/Source%20Code-ffa500?style=flat-square&logo=github&logoColor=fff&label=Github&labelColor=333" /></a></div>
         </div>
     );
 }
