2c5cded0836cdbda843d98929e117638403e5084
Souravdey777 <piyush.kolkata@gmail.com>
2021-02-17 02:50:58 +0530

    🚩 links added

--- src/Components/FlexHeading.js
+++ src/Components/FlexHeading.js
@@ -5,7 +5,30 @@ function FlexHeading() {
     return (
         <div className={Styles.rootFlexHeading}>
             <div className={Styles.pageHeading}>Flexbox-Guide</div>
-            <div className={Styles.pageSubHeading}>By <a className={Styles.link} href="https://github.com/Souravdey777" target="_blank" rel="noreferrer">Sourav Dey</a></div>
+            <div className={Styles.pageSubHeading}>Developed with love by <a className={Styles.link} href="https://github.com/Souravdey777" target="_blank" rel="noreferrer">@Souravdey777</a></div>
+            <div className={Styles.social_icons}>
+                <a
+                    href="https://github.com/Souravdey777/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i className="fab fa-github"></i>
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/souravdey777/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i className="fab fa-linkedin"></i>
+                </a>
+                <a
+                    href="https://twitter.com/Souravdey777/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i className="fab fa-twitter"></i>
+                </a>
+            </div>
             <div className={Styles.pageDescription}>A Guide for the concept of <a className={Styles.link} href="https://www.w3schools.com/css/css3_flexbox_container.asp" target="_blank" rel="noreferrer">Flexbox</a> and responsive design. Set the attributes as needed and copy the CSS code.</div>
             <div className={Styles.pageDescription}>CSS Flexible Box Layout is a module of CSS that defines a CSS box model optimized for user interface design, and the layout of items in one dimension.</div>
             <div className={Styles.pageDescription}>Show some love <span className="material-icons" style={{ fontSize: "14px", margin: "0px -2px", verticalAlign: "-2px", color: "#ffa500" }}>star</span> Star the Repository and support the project</div>
